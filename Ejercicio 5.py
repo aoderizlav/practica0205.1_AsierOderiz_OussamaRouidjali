377efb0add858bee6ab7a4add8c4faf9788b0141
aoderizlav <113341236+aoderizlav@users.noreply.github.com>
2022-11-15 12:48:34 +0100

    Practica 5.1

---
+++ Ejercicio 5.py
@@ -0,0 +1,15 @@
+diccionario = {}
+palabra = []
+
+while True:
+    usr_palabra = input("Introduzca la palabara y su traduccion separados por dos puntos "
+                        "puede parar el programa con [Terminar]: \n")
+    if usr_palabra == "Terminar":
+        break
+    palabra = usr_palabra.split(":")
+    diccionario[palabra[0]] = palabra[1]
+    print(diccionario)
+frase = input("Introduzca una frase en Español para traducir al Ingles: \n")
+frase = frase.split(" ")
+for es in frase:
+    print(es.replace(es, diccionario[es]), end=" ")
---
+++ Ejercicio 6.py
@@ -0,0 +1,63 @@
+base_datos = [{"01": {"dsa": "sads", "sa": "lk"},
+               "02": {"ads": "True"}}]
+
+print("--------------------------------------------")
+print("            Seleccione una opcion:          ")
+print("--------------------------------------------")
+print("         [1] Nuevo alumno")
+print("         [2] Eliminar Alumno")
+print("         [3] Mostrar Alumno")
+print("         [4] Mostrar Base de Datos")
+print("         [5] Mostrar Lista de Aprobados")
+print("         [6] Termninar Programa")
+print("--------------------------------------------")
+
+while True:
+    opcion = int(input("Seleccione una opcion: \n"))
+    if opcion == 1:
+        nif = input("DNI del nuevo alumno:\n")
+        nombre = input("Nombre del nuevo alumno:\n")
+        apellidos = input("Apellidos del nuevo alumno: \n")
+        telefono = input("Numero de telefono del nuevo alumno: \n")
+        correo = input("Correo del nuevo alumno: \n")
+        aprobado = input("¿Ha aprobado el nuevo alumno?: True o False \n")
+        datos = {}
+        datos["nombre"] = nombre
+        datos["apellidos"] = apellidos
+        datos["telefono"] = telefono
+        datos["correo"] = correo
+        datos["aprobado"] = aprobado
+        dic_nif = {}
+        dic_nif[nif] = datos
+        base_datos.append(dic_nif)
+        print(base_datos)
+    elif opcion == 2:
+        x2 = input("NIF del Alumno que desea eleminar: \n")
+        for i in base_datos:
+            del i[x2]
+        print(base_datos)
+    elif opcion == 3:
+        x3 = input("NIF del Alumno que desea visualizar: \n")
+        for i in base_datos:
+            print(i[x3])
+    elif opcion == 4:
+        for i in base_datos:
+            print(i)
+    elif opcion == 5:
+        for i in base_datos:
+            if "True" in i:
+                print(i)
+            else:
+                print("No hay aprobados")
+    elif opcion == 6:
+        break
+
+
+
+
+
+
+
+
+
+
